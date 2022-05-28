d5dc00a2668c9fe964b2c1b498de5aac677556c4
joanossoubita23 <99089435+joanossoubita23@users.noreply.github.com>
2022-05-28 00:18:46 -0400

    Add files via upload

---
+++ code-satck-s3-bucket.sh
@@ -0,0 +1,10 @@
+
+    "Resources": {
+        "S3BF4XF": {
+            "Type": "AWS::S3::Bucket",
+            "Properties": {
+                "BucketName" :"joan-bucket-123"
+            }
+        }
+    }
+}
